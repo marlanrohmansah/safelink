15a47114c102c7156904683dc40fd3927d681929
marlanrohmansah <51943029+marlanrohmansah@users.noreply.github.com>
2019-06-19 00:29:54 +0700

    Create oke2.js

---
+++ oke2.js
@@ -0,0 +1,9 @@
+//<![CDATA[
+var setting = {
+exceptionurl : "google.com,twitter.com",
+path : "#?o="
+};
+//]]>
+</script>
+<script src='https://raw.githack.com/marlanrohmansah/safelink/master/bagi2.js'></script>
+<script src='https://www.blogger.com/feeds/4018580699545928263/posts/default?alt=json-in-script&max-results=150&callback=showurl'>
