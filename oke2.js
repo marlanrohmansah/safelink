e92db13d1bcf0efadc4c535739b691e353146e29
marlanrohmansah <51943029+marlanrohmansah@users.noreply.github.com>
2019-06-19 00:38:30 +0700

    Update oke2.js

--- oke2.js
+++ oke2.js
@@ -4,6 +4,6 @@ exceptionurl : "google.com,twitter.com",
 path : "#?o="
 };
 //]]>
-</script>
 <script src='https://raw.githack.com/marlanrohmansah/safelink/master/bagi2.js'></script>
-<script src='https://www.blogger.com/feeds/4018580699545928263/posts/default?alt=json-in-script&max-results=150&callback=showurl'>
+<script src='https://www.blogger.com/feeds/4018580699545928263/posts/default?alt=json-in-script&max-results=150&callback=showurl'></script>
+			
