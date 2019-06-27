8e4a3d2887be3464b6c0ed0e7355b38def3485e0
marlanrohmansah <51943029+marlanrohmansah@users.noreply.github.com>
2019-06-28 02:55:48 +0700

    Create bagi2oke.js

---
+++ bagi2oke.js
@@ -0,0 +1,10 @@
+<script type='text/javascript'>
+//<![CDATA[
+var setting = {
+exceptionurl : "facebook.com,telegram.com,google.com,twitter.com,t.me",
+path : "#?o="
+};
+//]]>
+</script>
+<script src='https://raw.githack.com/marlanrohmansah/safelink/master/bagi2.js' ></script>
+<script src='https://www.blogger.com/feeds/4018580699545928263/posts/default?alt=json-in-script&max-results=150&callback=showurl'></script>
